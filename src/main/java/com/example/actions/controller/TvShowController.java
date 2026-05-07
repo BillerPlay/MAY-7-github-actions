d496e6fb7c18bbfa9a74ef27aacf7c334b76230d
Marshall-Bits <kz_@hotmail.es>
2026-05-07 13:20:01 +0200

    sorry marcel, I've pushed to main

--- src/main/java/com/example/actions/controller/TvShowController.java
+++ src/main/java/com/example/actions/controller/TvShowController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tvshowsdsadas")
+@RequestMapping("/tvshows")
 public class TvShowController {
 
     private final TvShowService tvShowService;
